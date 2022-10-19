cf2a8bf5650dce8f94396919f95ddff14ff6b72c
Z0ro0 <chlrkdmf0805@gmail.com>
2022-10-19 13:06:23 +0900

    문서 로드 시 페이지가 열렸습니다 출력하기

---
+++ JS Dom Event/Event1.js
@@ -0,0 +1,10 @@
+//  window.onload를 사용하여 HTML 문서가 로드될 때 이벤트 핸들러가 작동하도록 해보세요.
+
+    // "페이지가 열렸습니다" 문구를 띄우는 함수를 작성하세요. 
+
+    window.onload =function() {
+        var text = document.getElementById("text");
+        text.innerHTML = "페이지가 열렸습니다";
+       };
+   
+   
